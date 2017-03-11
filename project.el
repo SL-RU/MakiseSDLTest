83f28370e9c30a6c201e56a111bd34c38a87e028
Alexander Lutsai <s.lyra@ya.ru>
2017-03-11 18:12:19 +0300

    remove local file

--- project.el
+++
@@ -1,6 +0,0 @@
-(ede-add-project-to-global-list
- (ede-compdb-project "MakiseSDLTest"
-		     :file "/home/lyra/prj/MakiseSDLTest/CMakeLists.txt"
-		     :build-command "cd /home/lyra/prj/MakiseSDLTest/build; make"
-		     :compdb-file "/home/lyra/prj/MakiseSDLTest/build/compile_commands.json")
-)
